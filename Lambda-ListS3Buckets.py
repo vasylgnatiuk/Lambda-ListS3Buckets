ee75329fb74a761b7fcc3ca9756f99cb0bfe86bd
Vasyl Gnatiuk <vasylgnatiuk@users.noreply.github.com>
2018-08-23 16:07:14 -0400

    Add files via upload

---
+++ Lambda-ListS3Buckets.py
@@ -0,0 +1,13 @@
+def lambda_handler(event, context):
+    import boto3, os
+
+    myS3 = boto3.client('s3')
+    try:
+    	results = myS3.list_buckets()
+    	print (results)
+    	output = ""
+    	for bucket in results['Buckets']:
+    		output = output + bucket['Name'] + "\n"
+    	print("<h1><font color=green>S3 Bucket List:</font></h1><br><br>" + output)
+    except:
+    	print("<h1><font color=red>Error!</font></h1><br><br>")
